4c590aee2b404b83e03f691a578700e50e9a2643
Rafaela Almeida Da Silva <rafaela.almeida.silva@gmail.com>
2021-01-05 15:09:48 -0500

    Add a link to GitHub

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/Rafalenda/react-weather-app"
+          target="_blank"
+        >
+          This site was coded by Rafaela
+        </a>{" "}
+        open sourced on GitHub
+      </footer>
+    </div>
+  );
 }
 
 export default App;
