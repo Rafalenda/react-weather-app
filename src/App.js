580b00cf70d998934d1a4f8ee6da31b8a5abfd8c
Rafaela Almeida Da Silva <rafaela.almeida.silva@gmail.com>
2021-01-08 14:43:40 -0500

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/Rafalenda/react-weather-app"
-          target="_blank"
-        >
-          This site was coded by Rafaela
-        </a>{" "}
-        open sourced on GitHub
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/Rafalenda/react-weather-app"
+            target="_blank"
+          >
+            This site was coded by Rafaela
+          </a>{" "}
+          open sourced on GitHub
+        </footer>
+      </div>
     </div>
   );
 }
