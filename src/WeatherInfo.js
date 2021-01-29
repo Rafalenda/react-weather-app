02f684cafc648858b417eb6897d96d6dc158a7e7
Rafaela Almeida Da Silva <rafaela.almeida.silva@gmail.com>
2021-01-29 14:52:02 -0500

    Convert temperature units

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -16,8 +17,8 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <img src={props.data.iconUrl} alt="imagem" className="float-left" />
-            <span className="temperature">{props.data.temperature}</span>
-            <span className="unit">°C </span>
+
+            <WeatherTemperature celsius={props.data.temperature} />
           </div>
         </div>
 
