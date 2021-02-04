1a2644a72fef84cd76673e592a7430a68073035c
Rafaela Almeida Da Silva <rafaela.almeida.silva@gmail.com>
2021-02-03 23:15:52 -0500

    Update icons for the forecast items

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -19,26 +19,46 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast row">
         <div className="col">
           {new Date(forecast.list[0].dt * 1000).getHours()}:00
+          <img
+            src={`https://openweathermap.org/img/wn/${forecast.list[0].weather[0].icon}@2x.png`}
+            alt=""
+          />
           {Math.round(forecast.list[0].main.temp)}°C
         </div>
 
         <div className="col">
           {new Date(forecast.list[1].dt * 1000).getHours()}:00
+          <img
+            src={`https://openweathermap.org/img/wn/${forecast.list[1].weather[0].icon}@2x.png`}
+            alt=""
+          />
           {Math.round(forecast.list[1].main.temp)}°C
         </div>
 
         <div className="col">
           {new Date(forecast.list[2].dt * 1000).getHours()}:00
+          <img
+            src={`https://openweathermap.org/img/wn/${forecast.list[2].weather[0].icon}@2x.png`}
+            alt=""
+          />
           {Math.round(forecast.list[2].main.temp)}°C
         </div>
 
         <div className="col">
           {new Date(forecast.list[3].dt * 1000).getHours()}:00
+          <img
+            src={`https://openweathermap.org/img/wn/${forecast.list[3].weather[0].icon}@2x.png`}
+            alt=""
+          />
           {Math.round(forecast.list[3].main.temp)}°C
         </div>
 
         <div className="col">
           {new Date(forecast.list[4].dt * 1000).getHours()}:00
+          <img
+            src={`https://openweathermap.org/img/wn/${forecast.list[4].weather[0].icon}@2x.png`}
+            alt=""
+          />
           {Math.round(forecast.list[4].main.temp)}°C
         </div>
       </div>
